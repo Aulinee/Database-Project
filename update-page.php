7529a81a8cc6bf08cc25868b276d9956ceb452d3
azrazuls <azrazuls@gmail.com>
2021-06-22 00:38:19 +0800

    Create update-page.php

---
+++ update-page.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style1.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Update Plan</title>
+    </head>
+
+    <body>
+        <header>
+            <img class="logo" src="TMFLIX Background2-02.png" alt="logo" width="80px" height="25px">
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Series</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <input type="text" placeholder="Search..">
+                </ul>
+            </nav>
+
+            <a class="user" href="#"><i img="user" src="user2.png" alt="user" width="20px" height="20px"></i>Gsebi</a>
+            <a class="signout" href="#">Sign Out</a>
+        </header>
+
+        <h1 class="update">Update Plan</h1>
+
+        <div class="standard">
+            <div class="column" style="background-color:#333333;">
+                <h3>Standard Plan</h3>
+            </div>
+            <div class="column" style="background-color:#333333;">
+                <h5>One device & Good Quality (480p)</h5>
+            </div>
+            <div class="column" style="background-color:#333333;">
+                <h4>RM 17/month</h4>
+            </div>
+        </div>
+
+        <div class="premium">
+            <div class="column" style="background-color:#333333;">
+                <h3>Premium Plan</h3>
+            </div>
+            <div class="column" style="background-color:#333333;">
+                <h5>Multiple device & Full HD (1080p)</h5>
+            </div>
+            <div class="column" style="background-color:#333333;">
+                <h4>RM 39/month</h4>
+            </div>
+        </div>
+    </body>
+</html>
